1989a401dc00eb4479f465d05ace3ad7361ee87e
Chiranjivi Amgai <gurkhalies@hotmail.com>
2020-04-30 17:53:55 -0400

    format

--- src/test/java/API/TC007_GET_Request_Authorization.java
+++ src/test/java/API/TC007_GET_Request_Authorization.java
@@ -10,36 +10,34 @@ import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 public class TC007_GET_Request_Authorization {
-	
+
 	@Test
 	void Authorization() {
-		//sepecify base URI
-				RestAssured.baseURI = "http://restapi.demoqa.com/authentication/CheckForAuthentication";
-				
-				
-				//basic authentication
-				PreemptiveBasicAuthScheme authscheme = new PreemptiveBasicAuthScheme();
-				authscheme.setUserName("ToolsQA");
-				authscheme.setPassword("TestPassword");
-				
-				RestAssured.authentication = authscheme;
-				
-				//request object
-				RequestSpecification httpRequest = RestAssured.given();
-				
-				//response object
-				Response response= httpRequest.request(Method.GET, "/");
-				//print response in the console
-				
-				String responseBody = response.getBody().asString();
-				System.out.println("responseBody is: "+responseBody);
-				
-				//validate status code
-				int statusCode = response.getStatusCode();
-				System.out.println("status code is : "+statusCode);
-				Assert.assertEquals(statusCode, 200);
-				
-			
+		// sepecify base URI
+		RestAssured.baseURI = "http://restapi.demoqa.com/authentication/CheckForAuthentication";
+
+		// basic authentication
+		PreemptiveBasicAuthScheme authscheme = new PreemptiveBasicAuthScheme();
+		authscheme.setUserName("ToolsQA");
+		authscheme.setPassword("TestPassword");
+
+		RestAssured.authentication = authscheme;
+
+		// request object
+		RequestSpecification httpRequest = RestAssured.given();
+
+		// response object
+		Response response = httpRequest.request(Method.GET, "/");
+		// print response in the console
+
+		String responseBody = response.getBody().asString();
+		System.out.println("responseBody is: " + responseBody);
+
+		// validate status code
+		int statusCode = response.getStatusCode();
+		System.out.println("status code is : " + statusCode);
+		Assert.assertEquals(statusCode, 200);
+
 	}
 
 }
